01bef9f0a03a1bffeaaeee110701d64aa03ed88e
AlhHabibah <86188075+AlhHabibah@users.noreply.github.com>
2021-09-30 04:55:59 +0300

    Update setup.sh

--- starter/setup.sh
+++ starter/setup.sh
@@ -1,1 +1,6 @@
-
+DATABASE_URL='postgresql://postgres:Habibah1234&@localhost:5432/castagency'
+AUTH0_DOMAIN='udacity-habibah.us.auth0.com/'
+ALGORITHMS='RS256'
+API_AUDIENCE='CastAgency'
+FLASK_APP=app.py
+FLASK_ENV=development
